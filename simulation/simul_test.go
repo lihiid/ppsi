2b8c09ed8d8bc203c99574f2f6aa91038afa9567
lihiid <lihi.idan@yale.edu>
2017-02-09 11:23:30 -0800

    Add files via upload

--- simulation/simul_test.go
+++ simulation/simul_test.go
@@ -2,15 +2,9 @@ package main_test
 
 import (
 	"testing"
-
-	"gopkg.in/dedis/onet.v1/log"
-	"gopkg.in/dedis/onet.v1/simul"
+	"github.com/dedis/onet/simul"
 )
 
-func TestMain(m *testing.M) {
-	log.MainTest(m)
-}
-
 func TestSimulation(t *testing.T) {
-	simul.Start("protocol.toml", "service.toml")
+	simul.Start("ppsi.toml")
 }
